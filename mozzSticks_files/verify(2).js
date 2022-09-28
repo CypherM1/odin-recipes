eead57b11fbffdfc1f4e29bbed58b0b36cfbb62f
Justin Sears <justinrsears@gmail.com>
2022-09-27 22:29:04 -0700

    Change image src on all sites

---
+++ mozzSticks_files/verify(2).js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_235058105072({ImpressionID:"b6100abb5c5849d486e3acd150b366ff", ServerPublicDns:"rtbc-uw2.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "b6100abb5c5849d486e3acd150b366ff");}catch(e){}
+try{__verify_callback_235058105072({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "b6100abb5c5849d486e3acd150b366ff");}catch(e){}
---
+++ mozzSticks_files/verify(4).js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_108416851624({ImpressionID:"b42cd8cee4b64d95b1d0173221bc65a4", ServerPublicDns:"rtbc-uw2.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "b42cd8cee4b64d95b1d0173221bc65a4");}catch(e){}
+try{__verify_callback_108416851624({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "b42cd8cee4b64d95b1d0173221bc65a4");}catch(e){}
